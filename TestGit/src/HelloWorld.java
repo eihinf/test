c5821c3494f2659afba9bfdffb32c7338ef92226
Eihab Khadora <eihab@smartway-me.com>
2015-03-24 16:58:39 +0300

    commited

---
+++ TestGit/src/HelloWorld.java
@@ -0,0 +1,16 @@
+
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("here from eihab");
+		for(int i=0;i<10;i++){
+			System.out.println("Test");
+		}
+		String[] s = {"hello","test"};
+
+	}
+
+}
